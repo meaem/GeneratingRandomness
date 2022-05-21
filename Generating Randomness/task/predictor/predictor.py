535d2367c31b0ad635a2796e919b847e93d826b1
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-05-21 16:38:54 +0200

    stage 2 finished

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -1,4 +1,17 @@
-
+def count_pattern(user_str,pattern):
+    last_idx = user_str.find(pattern)
+    zero_count = 0
+    one_count = 0
+    p_len = len(pattern)
+    user_str_len = len(user_str)
+    while last_idx != -1 and last_idx+p_len < user_str_len:
+        if user_str[last_idx+p_len] == '0':
+            zero_count +=1
+        else: #if user_str[last_idx+p_len] == '1':
+            one_count +=1
+        last_idx +=1 # p_len
+        last_idx=user_str.find(pattern,last_idx)
+    return  zero_count,one_count
 
 
 def main():
@@ -14,6 +27,11 @@ def main():
         if 100-len(user_str) > 0:
             print(f"Current data length is {len(s)}, {100-len(user_str)} symbols left")
     print(f"Final data string:\n{user_str}")
-
+    for x in ['0','1']:
+        for y in ['0','1']:
+            for z in ['0','1']:
+                pattern = f"{x}{y}{z}"
+                zero_after,one_after = count_pattern(user_str,pattern)
+                print(f"{pattern}: {zero_after},{one_after}")
 
 main()
